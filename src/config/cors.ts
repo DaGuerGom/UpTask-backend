55199a97f5aa5b93c6dd84c6822d8e0fe9262496
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2024-09-09 18:15:23 +0200

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,6 @@ import { CorsOptions } from 'cors'
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
         const whitelist = [process.env.FRONTEND_URL]
-        console.log("origin: "+origin)
         if(process.argv[2] === '--api') {
             whitelist.push(undefined)
         }
@@ -11,7 +10,8 @@ export const corsConfig: CorsOptions = {
         if(whitelist.includes(origin)) {
             callback(null, true)
         } else {
-            callback(new Error('Error de CORS'))
+            console.log("origin: "+origin)
+            //callback(new Error('Error de CORS'))
         }
     }
 }
