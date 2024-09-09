08c3a7a42aec0656cbd23e759be949fa70b2f13e
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2024-09-09 18:08:13 +0200

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from 'cors'
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
         const whitelist = [process.env.FRONTEND_URL]
-        console.log("env: "+process.env.FRONTEND_URL)
+        console.log("origin: "+origin)
         if(process.argv[2] === '--api') {
             whitelist.push(undefined)
         }
